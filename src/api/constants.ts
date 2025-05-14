e96aaca8badf094a21e3b782270e436d99195a0e
PremKumar <109847674+prem2230@users.noreply.github.com>
2025-05-14 23:20:37 +0530

    refactor: remove default values for API_URL and APP_TITLE in configuration

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -2,9 +2,9 @@ const isDevelopment = import.meta.env.DEV;
 
 export const CONFIG = {
     ENV: import.meta.env.MODE,
-    API_URL: import.meta.env.VITE_API_URL || 'http://localhost:4001/api',
-    APP_TITLE: import.meta.env.VITE_APP_TITLE || 'Quiz App',
+    API_URL: import.meta.env.VITE_API_URL,
+    APP_TITLE: import.meta.env.VITE_APP_TITLE,
     IS_DEV: isDevelopment,
 }
 export const APPLICATION_NAME: string = CONFIG.APP_TITLE;
-export const BASE_URL: string =  CONFIG.API_URL;
+export const BASE_URL: string = CONFIG.API_URL;
